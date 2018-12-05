c85ff34c47cd4fa388998f233b1d321957c65e3e
saBasKouw <36665806+saBasKouw@users.noreply.github.com>
2018-12-05 21:22:43 +0100

    Add files via upload

--- DiagnoseAddictionApp/app.js
+++ DiagnoseAddictionApp/app.js
@@ -1,132 +1,173 @@
 
-let questions = [];
+
+let domainIndex = 0;
+let domains = Object.keys(questionData["domain"]);
+let results = [];
+
+function populateAll(){
 
 
-for(let question of questionData["domain"]["DSM5"]){
-    questions.push(new Question(question['text'], question['choices']));
-}
-
-let quiz = new Quiz(questions);
-
-let resultIsSent = false;
-
-
-
+    let element = document.getElementById("quiz");
+    element.innerHTML = "<h1>"+domains[domainIndex]+"</h1>\n" +
+        "        <hr style=\"margin-top: 20px\">\n" +
+        "\n" +
+        "        <p id=\"question\"></p>\n" +
+        "\n" +
+        "        <div class=\"buttons\">\n" +
+        "        </div>";
 
 
 
-function chooseAnswer() {
-    for (let i = 0; i < quiz.getQuestionWithIndex().choices.length; i++) {
-        let button = document.getElementById('btn'+i);
-        button.onclick = ()=> {
-            quiz.guess(quiz.getQuestionWithIndex().choices[i]);
-            $( ".buttons" ).empty();
+    let questions = [];
+    let quiz;
+
+    populateQuiz();
+
+    let resultIsSent = false;
+
+
+    function populateQuiz(){
+        for(let question of questionData["domain"][domains[domainIndex]]){
+            questions.push(new Question(question['text'], question['choices']));
+        }
+        quiz = new Quiz(questions);
+    }
+
+
+    function chooseAnswer() {
+        for (let i = 0; i < quiz.getQuestionWithIndex().choices.length; i++) {
+            let button = document.getElementById('btn'+i);
+            button.onclick = ()=> {
+                quiz.guess(quiz.getQuestionWithIndex().choices[i]);
+                $( ".buttons" ).empty();
+                populate();
+
+            }
+        }
+    }
+
+    function resultDSM(result){
+        let count = 0;
+        for(let answer of result){
+            if(answer === "yes"){
+                count++;
+            }
+        }
+        result = [];
+        if(count < 2){
+            result = "No indication of alcohol abuse";
+        } else if(count < 4){
+            result = "Mild indication of alcohol abuse";
+        } else if(count < 6){
+            result = "Moderate indication of alcohol abuse";
+        }  else if(count >= 6){
+            result = "Severe indication of alcohol abuse";
+        } results.push(result);
+    }
+
+    function whichResult(result){
+        if(domains[domainIndex] === "DSM5"){
+            resultDSM(result);
+        } else if(domains[domainIndex] === "background") {
+            console.log("background result");
+        }
+    }
+
+
+    function sendResult(){
+        $("#sendResult").click(()=> {
+            resultIsSent = true;
+            let result = [];
+            for(let i = 0; i < questions.length; i++){
+                for(let j = 0; j < questions[i].choices.length; j++){
+                    let element = document.getElementById(i.toString() +"-"+ j.toString());
+                    if(element.checked){
+                        result.push(questions[i].choices[j]);
+                    }
+                }
+            }
+            whichResult(result);
             populate();
+        });
+    }
 
+    function populateQuestion(){
+        let element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionWithIndex().text;
+        let newHtml = '';
+        for(let i = 0; i < quiz.getQuestionWithIndex().choices.length; i++){
+            newHtml += "<button class='btn' id='btn"+i+"'><span id='choice"+i+"'>"+quiz.getQuestionWithIndex().choices[i]+"</span></button>"
         }
+        $('.buttons').append(newHtml);
+
+        chooseAnswer();
     }
-}
 
-function resultDSM(result){
-    let count = 0;
-    for(let answer of result){
-        if(answer === "yes"){
-            count++;
-        }
+
+
+
+
+    function showProgress(){
+        let currentQuestionNumber = quiz.questionIndex + 1;
+        let element = document.getElementById("progress");
+        element.innerHTML = "Question "+ currentQuestionNumber+" of "+quiz.questions.length;
     }
-    if(count < 2){
-        console.log("No indication of alcohol abuse");
-    } else if(count < 4){
-        console.log("Mild indication of alcohol abuse");
-    } else if(count < 6){
-        console.log("Moderate indication of alcohol abuse");
-    }  else if(count >= 6){
-        console.log("Severe indication of alcohol abuse");
-    }
-}
 
-
-
-
-function sendResult(){
-    $("#sendResult").click(()=> {
-        resultIsSent = true;
-        let result = [];
+    function showResult(){
+        let gameOverHtml = "<h1>Revise</h1>";
         for(let i = 0; i < questions.length; i++){
+            gameOverHtml += "<p>"+ questions[i].text +"</p>";
             for(let j = 0; j < questions[i].choices.length; j++){
-                let element = document.getElementById(i.toString() +"-"+ j.toString());
-                if(element.checked){
-                    result.push(questions[i].choices[j]);
+                let id = i.toString() +"-"+ j.toString();
+                if(questions[i].isSame(questions[i].choices[j])){
+                    gameOverHtml += "<div> <input type='radio' name=question"+i+" id="+ id +" value=" + id + " checked> <label for="+id+">"+questions[i].choices[j]+"</label> </div>"
+                } else {
+                    gameOverHtml += "<div> <input type='radio' name=question"+i+" id="+ id+" value=" + id + "> <label for="+id+">"+questions[i].choices[j]+"</label> </div>"
                 }
             }
         }
-        resultDSM(result);
-        populate();
-    });
-}
+        let element = document.getElementById("quiz");
+        element.innerHTML = gameOverHtml;
 
-function populateQuestion(){
-    let element = document.getElementById("question");
-    element.innerHTML = quiz.getQuestionWithIndex().text;
-    let newHtml = '';
-    for(let i = 0; i < quiz.getQuestionWithIndex().choices.length; i++){
-        newHtml += "<button id='btn"+i+"'><span id='choice"+i+"'>"+quiz.getQuestionWithIndex().choices[i]+"</span></button>"
+        element = document.getElementById("progress");
+        if(domainIndex === domains.length-1){
+            element.innerHTML = "<button id='sendResult'><span>Sent result to doctor</span></button>";
+        } else {
+            element.innerHTML = "<button id='sendResult'><span>Next</span></button>";
+        }
+
     }
-    $('.buttons').append(newHtml);
 
-    chooseAnswer();
-}
+    function messageSent(){
+        let element = document.getElementById("quiz");
+        let gameOverHtml = "<h1>Result</h1>";
+        gameOverHtml += "<h2>Thank you for participating. The results are sent to the doctor.</h2>"
+        element.innerHTML = gameOverHtml;
+        element = document.getElementById("progress");
+        element.innerHTML = "";
+    }
 
 
 
-
-
-function showProgress(){
-    let currentQuestionNumber = quiz.questionIndex + 1;
-    let element = document.getElementById("progress");
-    element.innerHTML = "Question "+ currentQuestionNumber+" of "+quiz.questions.length;
-}
-
-function showResult(){
-    let gameOverHtml = "<h1>Result</h1>";
-    for(let i = 0; i < questions.length; i++){
-        gameOverHtml += "<p>"+ questions[i].text +"</p>";
-        for(let j = 0; j < questions[i].choices.length; j++){
-            let id = i.toString() +"-"+ j.toString();
-            if(questions[i].isSame(questions[i].choices[j])){
-                gameOverHtml += "<div> <input type='radio' name=question"+i+" id="+ id +" value=" + id + " checked> <label for="+id+">"+questions[i].choices[j]+"</label> </div>"
+    function populate(){
+        if(quiz.isEnded() && !resultIsSent){
+            showResult();
+            sendResult();
+        } else if(resultIsSent) {
+            if (domainIndex === domains.length-1) {
+                messageSent();
             } else {
-                gameOverHtml += "<div> <input type='radio' name=question"+i+" id="+ id+" value=" + id + "> <label for="+id+">"+questions[i].choices[j]+"</label> </div>"
+                domainIndex++
+                populateAll();
             }
+        } else {
+            populateQuestion();
+            showProgress();
         }
     }
-    let element = document.getElementById("quiz");
-    element.innerHTML = gameOverHtml;
 
-    element = document.getElementById("progress");
-    element.innerHTML = "<button id='sendResult'><span>Send result to doctor</span></button>";
-}
-
-function messageSent(){
-    let element = document.getElementById("quiz");
-    let gameOverHtml = "<h1>Result</h1>";
-    gameOverHtml += "<h2>Thank you for participating. The results are sent to the doctor.</h2>"
-    element.innerHTML = gameOverHtml;
-    element = document.getElementById("progress");
-    element.innerHTML = "";
-}
-
-function populate(){
-    if(quiz.isEnded() && !resultIsSent){
-        showResult();
-        sendResult();
-    } else if(resultIsSent){
-        messageSent();
-    } else {
-        populateQuestion();
-        showProgress();
-    }
+    populate();
 }
 
 
-populate();
+populateAll();
--- DiagnoseAddictionApp/questions.js
+++ DiagnoseAddictionApp/questions.js
@@ -1,5 +1,13 @@
 let questionData = {"domain":
-        {
+        {   "Determine addiction": [{
+                "text": "Had times when you ended up drinking more, or longer, than you intended?",
+                "choices": ["yes", "no"]
+            },
+                {
+                    "text": "More than once wanted to cut down or stop drinking, or tried to, but couldn’t?",
+                    "choices": ["yes", "no"]
+                },
+                ],
             "DSM5": [{
                 "text": "Had times when you ended up drinking more, or longer, than you intended?",
                 "choices": ["yes", "no"]
@@ -8,44 +16,138 @@ let questionData = {"domain":
                     "text": "More than once wanted to cut down or stop drinking, or tried to, but couldn’t?",
                     "choices": ["yes", "no"]
                 },
-                {
-                    "text": "Spent a lot of time drinking? Or being sick or getting over other after effects?",
-                    "choices": ["yes", "no"]
-                },
-                {
-                    "text": "Wanted a drink so badly you couldn’t think of anything else?",
-                    "choices": ["yes", "no"]
-                },
-                {
-                    "text": "Found that drinking—or being sick from drinking—often interfered with taking care of your home or family? Or caused job troubles? Or school problems?",
-                    "choices": ["yes", "no"]
-                },
-                {
-                    "text": "Continued to drink even though it was causing trouble with your family or friends?",
-                    "choices": ["yes", "no"]
-                },
-                {
-                    "text": "Given up or cut back on activities that were important or interesting to you, or gave you pleasure, in order to drink?",
-                    "choices": ["yes", "no"]
-                },
-                {
-                    "text": "More than once gotten into situations while or after drinking that increased your chances of getting hurt (such as driving, swimming, using machinery, walking in a dangerous area, or having unsafe sex)?",
-                    "choices": ["yes", "no"]
-                },
-                {
-                    "text": "Continued to drink even though it was making you feel depressed or anxious or adding to another health problem? Or after having had a memory blackout?",
-                    "choices": ["yes", "no"]
-                },
+                // {
+                //     "text": "Spent a lot of time drinking? Or being sick or getting over other after effects?",
+                //     "choices": ["yes", "no"]
+                // },
+                // {
+                //     "text": "Wanted a drink so badly you couldn’t think of anything else?",
+                //     "choices": ["yes", "no"]
+                // },
+                // {
+                //     "text": "Found that drinking—or being sick from drinking—often interfered with taking care of your home or family? Or caused job troubles? Or school problems?",
+                //     "choices": ["yes", "no"]
+                // },
+                // {
+                //     "text": "Continued to drink even though it was causing trouble with your family or friends?",
+                //     "choices": ["yes", "no"]
+                // },
+                // {
+                //     "text": "Given up or cut back on activities that were important or interesting to you, or gave you pleasure, in order to drink?",
+                //     "choices": ["yes", "no"]
+                // },
+                // {
+                //     "text": "More than once gotten into situations while or after drinking that increased your chances of getting hurt (such as driving, swimming, using machinery, walking in a dangerous area, or having unsafe sex)?",
+                //     "choices": ["yes", "no"]
+                // },
+                // {
+                //     "text": "Continued to drink even though it was making you feel depressed or anxious or adding to another health problem? Or after having had a memory blackout?",
+                //     "choices": ["yes", "no"]
+                // },
+                //
+                // {
+                //     "text": "Had to drink much more than you once did to get the effect you want? Or found that your usual number of drinks had much less effect than before?",
+                //     "choices": ["yes", "no"]
+                // },
+                // {
+                //     "text": "Found that when the effects of alcohol were wearing off, you had withdrawal symptoms, such as trouble sleeping, shakiness, restlessness, nausea, sweating, a racing heart, or a seizure? Or sensed things that were not there?",
+                //     "choices": ["yes", "no"]
+                // }
 
+            ],
+            "Treatment history": [{
+                "text": "Which of the following best describe your social living situation?",
+                "choices": ["I am single", " I am living alone", "With partner with children", "With partner without children", "Single with child(ren)", "I am a child living with a single parent", "I am a child living with a multiple parents", "I am living in a GGZ clinic", "I live with others in a clinic", "I am living in a institution that is not GGZ", "I am living with youth services", "I am homeless", "Pension", "other"]
+            },
                 {
-                    "text": "Had to drink much more than you once did to get the effect you want? Or found that your usual number of drinks had much less effect than before?",
+                    "text": "Which of the following best describe your living situation?",
+                    "choices": ["otherwise, incl. asylum seeker center", "own home", "living with family/friends", "guest family", "GGZ-institution", "renting a room", "living with my parents", "living with foster family", "TBS-institution", "other institutional facility", "other judicial institution", "homeless shelter", "addiction clinic", "nursing home", "homeless"]
+                },
+                // {
+                //     "text": "What is your cultural heritage?",
+                //     "choices": ["Dutch", "Antillean", "Moluccan", "Moroccan", "Turkish", "Surinamese", "mixed", "East-European", "South-European", "other European", "Other Asian", "Other South-America", "other industrialized country", "other non-industrialized country", "other African"]
+                // },
+                // {
+                //     "text": "Which source of income do you have?",
+                //     "choices": ["salary", "alimony", "retirement fund", "other", "social service"]
+                // },
+                // {
+                //     "text": "What is the highest education you completed?",
+                //     "choices": ["university", "special education", "Higher secondary education", "lower secondary education", "middle secondary education", "other"]
+                // },
+                // {
+                //     "text": "How much have you worked the last 5 years?",
+                //     "choices": ["one year", "two years", "three years", "four years", "five years", "less than a year"]
+                // },
+                // {
+                //     "text": "With which of the following complaints do you seek treatment?",
+                //     "choices": ["general feeling unwell", "fear and tension", "being a threat to myself", "being homeless", "behavioural complaints", "no complaints of my own", "memory orientation cognitive", "socializing with others", "identity complaints", "incest", "substance use", "struggling with sexual orientation", "upbringing", "psychosomatic, physical", "abuse(children)", "abuse(adults)", "had a traumatic event", "can not explain", "unknown", "being psychotic", "partner/relationship/family problems", "I am mourning", "have trouble paying attention/school problems", "feeling gloom", "mood complaints", "being addicted", "being neglected/neglecting myself", "run away from home", "work/study problems"]
+                // },
+                // {
+                //     "text": "To which substance are you addicted?",
+                //     "choices": ["Alcohol", "Tobacco", "Cannabis"]
+                // },
+                // {
+                //     "text": "How many times do you use?",
+                //     "choices": ["multiple times a day", "daily", "multiple times a week", "weekly", "irregular"]
+                // },
+                //
+                // {
+                //     "text": "In what way do you ingest the drug that you use?",
+                //     "choices": ["drinking", "smoking", "snorting", "injecting", "other"]
+                // },
+                // {
+                //     "text": "How long have you been using this drug",
+                //     "choices": ["1 to 3 months", "3 to 6 months", "6 to 12 months", "1 to 2 years", "2 to 5 years", "5 to 10 years", "more than 10 years"]
+                // }
+
+            ],
+            "family addiction situation": [{
+                "text": "Had times when you ended up drinking more, or longer, than you intended?",
+                "choices": ["yes", "no"]
+            },
+                {
+                    "text": "More than once wanted to cut down or stop drinking, or tried to, but couldn’t?",
                     "choices": ["yes", "no"]
                 },
+            ],
+            "Medicine intake": [{
+                "text": "Had times when you ended up drinking more, or longer, than you intended?",
+                "choices": ["yes", "no"]
+            },
                 {
-                    "text": "Found that when the effects of alcohol were wearing off, you had withdrawal symptoms, such as trouble sleeping, shakiness, restlessness, nausea, sweating, a racing heart, or a seizure? Or sensed things that were not there?",
+                    "text": "More than once wanted to cut down or stop drinking, or tried to, but couldn’t?",
                     "choices": ["yes", "no"]
-                }
+                },
+            ],
+            "Treatment wishes": [{
+                "text": "Had times when you ended up drinking more, or longer, than you intended?",
+                "choices": ["yes", "no"]
+            },
+                {
+                    "text": "More than once wanted to cut down or stop drinking, or tried to, but couldn’t?",
+                    "choices": ["yes", "no"]
+                },
+            ],
+            "Psychological health": [{
+                "text": "Had times when you ended up drinking more, or longer, than you intended?",
+                "choices": ["yes", "no"]
+            },
+                {
+                    "text": "More than once wanted to cut down or stop drinking, or tried to, but couldn’t?",
+                    "choices": ["yes", "no"]
+                },
+            ],
+            "Social situation": [{
+                "text": "Had times when you ended up drinking more, or longer, than you intended?",
+                "choices": ["yes", "no"]
+            },
+                {
+                    "text": "More than once wanted to cut down or stop drinking, or tried to, but couldn’t?",
+                    "choices": ["yes", "no"]
+                },
+            ],
 
-            ]}
+        }
 
     };
